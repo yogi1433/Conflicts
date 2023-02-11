0dade552fe6b467f7cd4e63c0341024b3033bf1c
yogi1433 <121537038+yogi1433@users.noreply.github.com>
2023-02-11 18:05:16 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -1,6 +1,8 @@
-
-public static void main(String[] args)
-{
-disp();
-}
-}
+public class Sample{
+  static void mul()
+  {
+    int a=5;
+    int b=5;
+    int c=a*b;
+    System.out.println(c);
+  }
